97ed727a0bddc5a671867b3240cbd92a8396f3c0
Rahul Belgaonkar <58828810+Rahuleus12@users.noreply.github.com>
2025-03-21 11:29:28 +0530

    Add files via upload

---
+++ test_calculate.py
@@ -0,0 +1,20 @@
+import pytest
+from calculate import add,subtract,divide,multiply
+
+def test_add():
+    assert add(2,3)==5
+    assert add(4,5)==9
+
+def test_subtract():
+    assert subtract(3,2)==1
+    assert subtract(4,4)==0
+
+def test_multiply():
+    assert multiply(2,3)==6
+    assert multiply(4,5)==20
+
+def test_divide():
+    assert divide(27,3)==9
+    assert divide(42,14)==3
+
+
